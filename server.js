5de4c720c19ec3ce2a70cb2e0ff7c4021e1834db
Hongfei01 <hongfeidu3@gmail.com>
2024-08-08 08:55:34 +0800

    modify main.jsx & package.json command

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // use static folder
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 // test
 app.get('/', (req, res) => {
@@ -58,7 +58,7 @@ app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 // not found
